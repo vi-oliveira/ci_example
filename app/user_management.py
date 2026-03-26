09674dfb339080d971055e1af3306f69a4d7b6d7
Vinícius de Oliveira Silva <Vinyoliveira2005@gmail.com>
2026-03-26 20:55:55 -0300

    returning an ordered list

--- app/user_management.py
+++ app/user_management.py
@@ -26,7 +26,7 @@ def get_user(user_id: int) -> Optional[Dict]:
 
 
 def list_users() -> List[Dict]:
-    return _db.list()
+    return sorted(_db.list(), key = lambda x: x["email"])
 
 
 def delete_user(user_id: int) -> bool:
